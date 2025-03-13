4c3e3e98cac2bd662764684aad32b9508be7ffd3
Heaves7 <elouanekoka95@hotmail.com>
2025-03-13 16:26:23 +0100

    J'en ai marre

---
+++ frontend/frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
